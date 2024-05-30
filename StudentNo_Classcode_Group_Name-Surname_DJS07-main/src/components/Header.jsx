71bc6f5544f5ec1e93ec73ebf064d3bc95b28f04
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-05-30 13:19:56 +0200

    Created Header component and added basic styling

--- StudentNo_Classcode_Group_Name-Surname_DJS07-main/src/components/Header.jsx
+++ StudentNo_Classcode_Group_Name-Surname_DJS07-main/src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react"
 
 export default function Header() {
-    return ( 
-    <h1>Header coponent!</h1>
-)
+    return (
+        <header className="header">
+            <img 
+                src="./images/troll-face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
 }
