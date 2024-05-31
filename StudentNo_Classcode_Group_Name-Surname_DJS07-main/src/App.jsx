e584425b9f61a4eb133075892aba9cbfd3dc48dc
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-05-31 09:54:26 +0200

    Implemented Meme component with state management

--- StudentNo_Classcode_Group_Name-Surname_DJS07-main/src/App.jsx
+++ StudentNo_Classcode_Group_Name-Surname_DJS07-main/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "./components/Header"
 import Meme from "./components/Meme"
-
+import memesData from "./components/memesData"
 function App() {
 
   return (
